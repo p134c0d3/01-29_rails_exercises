a27f1271a3246c7327cde91d3c3283669e6f1e35
p134c0d3 <70922864+p134c0d3@users.noreply.github.com>
2024-01-29 23:31:44 -0600

    Exercises Update

--- product_api/app/models/product.rb
+++ product_api/app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
+  validates :price, numericality: { only_numeric: true, greater_than: 0 }
 end
